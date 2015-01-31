8b346db33f34c414b054e9d065fd96c7f3fae740
DatRoyce <roycewhitaker@rocketmail.com>
2015-01-30 18:37:09 -0800

    Final 1/30/2015

--- Test 2/manual.c
+++ Test 2/manual.c
@@ -82,63 +82,63 @@ void initializeRobot()
 
 task main()
 {
-    initialize ();  // In the original template this was "initializeRobot();"
+	initialize ();  // In the original template this was "initializeRobot();"
 
-    waitForStart ();   // wait for start of tele-op phase
+	waitForStart ();   // wait for start of tele-op phase
 
-    while (true)
-    {
-        //
-        // Read the current values of the joystick controllers.
-        //
-        hogCPU (); // Prevent the messaging task from writing to it while it is being read.
-        getJoystickSettings (joystick); // Added to template for kick-off presentation
-        releaseCPU ();
+	while (true)
+	{
+		//
+		// Read the current values of the joystick controllers.
+		//
+		hogCPU (); // Prevent the messaging task from writing to it while it is being read.
+		getJoystickSettings (joystick); // Added to template for kick-off presentation
+		releaseCPU ();
 
-        //
-        // Manage power to the drive wheels.
-        //
-        motor[c_left_foot_motor] =
-            convert_joystick_to_motor (joystick.joy1_y1);
+		//
+		// Manage power to the drive wheels.
+		//
+		motor[c_left_foot_motor] =
+		convert_joystick_to_motor (joystick.joy1_y1);
 
-        motor[c_right_foot_motor] =
-            convert_joystick_to_motor (joystick.joy1_y2);
+		motor[c_right_foot_motor] =
+		convert_joystick_to_motor (joystick.joy1_y2);
 
-        //
-        // Manage the power and direction of the LEGO arm motors, which make the
-        // arm move up and down.
-        //
-        int l_motor = convert_joystick_to_motor (joystick.joy2_y1);
-        motor[c_left_arm_motor] = l_motor;
-        motor[c_right_arm_motor] = l_motor;
+		//
+		// Manage the power and direction of the LEGO arm motors, which make the
+		// arm move up and down.
+		//
+		int l_motor = convert_joystick_to_motor (joystick.joy2_y1);
+		motor[c_left_arm_motor] = l_motor;
+		motor[c_right_arm_motor] = l_motor;
 
-        //
-        // Manage the hand servo positions, which make the hand open and close.
-        //
-        if (joy2Btn (6) != false)
-        {
-            move_hand (e_hand_command_open);
-        }
-        else if (joy2Btn (8) != false)
-        {
-            move_hand (e_hand_command_close);
-        }
-        else if (joy2Btn (5) != false)
-        {
-            move_hand (e_hand_command_partially_open);
-        }
-        else if (joy2Btn (7) != false)
-        {
-            move_hand (e_hand_command_partially_close);
-        }
+		//
+		// Manage the hand servo positions, which make the hand open and close.
+		//
+		if (joy2Btn (6) != false)
+		{
+			move_hand (e_hand_command_open);
+		}
+		else if (joy2Btn (8) != false)
+		{
+			move_hand (e_hand_command_close);
+		}
+		else if (joy2Btn (5) != false)
+		{
+			move_hand (e_hand_command_partially_open);
+		}
+		else if (joy2Btn (7) != false)
+		{
+			move_hand (e_hand_command_partially_close);
+		}
 
-        //
-        // This wait is important for several reasons.  It provides the message
-        // a chance to perform its processing.  It also determines how fast the
-        // servos open and close (see the increment blocks in move_hand).
-        //
-        wait1Msec (10);
+		//
+		// This wait is important for several reasons.  It provides the message
+		// a chance to perform its processing.  It also determines how fast the
+		// servos open and close (see the increment blocks in move_hand).
+		//
+		wait1Msec (10);
 
-    } // while (true)
+	} // while (true)
 
 } // main
